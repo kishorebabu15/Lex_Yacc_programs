87d93f760894cf5b0f4269a44b3f5e32a267692b
Kishore Babu <polurukishorebabu@gmail.com>
2022-01-31 11:56:38 +0530

    LEX programs and input files

---
+++ input.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    // To prinit hello world
+
+    /* This is  a single line comment */
+
+    /* This is a 
+    multi line 
+    comment */
+
+    printf("Hello world !!!");
+    return 0;
+}
